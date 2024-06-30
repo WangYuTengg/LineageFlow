f6b4ad139843644dd4064001f514cd99aab8657c
WangYuTengg <wyuteng123@gmail.com>
2024-06-30 22:27:45 +0800

    placeholder endpoint for create repo

--- server/myapp/urls.py
+++ server/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HelloWorldView
+from .views import CreateRepositoryView
 
 urlpatterns = [
     path("hello/", HelloWorldView.as_view(), name="hello-world"),
+    path("createRepository/", CreateRepositoryView.as_view(), name="create-repository"),
 ]
--- server/myapp/views.py
+++ server/myapp/views.py
@@ -6,3 +6,9 @@ from rest_framework import status
 class HelloWorldView(APIView):
     def get(self, request):
         return Response("Hello world", status=status.HTTP_200_OK)
+
+
+class CreateRepositoryView(APIView):
+    def post(self, request):
+        # This is a placeholder for creating a repository
+        return Response("Repository created", status=status.HTTP_201_CREATED)
