88ec2ae6b1ddbf1c61d261fb7b35eeb2481a7e87
WangYuTengg <wyuteng123@gmail.com>
2024-07-02 21:37:38 +0800

    models: add uuid generation

--- server/myapp/models.py
+++ server/myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import uuid
+
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
@@ -7,6 +9,7 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
+
 class Files(models.Model):
     url = models.URLField(primary_key=True)
     meta_data = models.TextField()
@@ -14,47 +17,56 @@ class Files(models.Model):
     def __str__(self):
         return self.url
 
+
 class Range(models.Model):
-    range_id = models.CharField(max_length=10, primary_key=True)
+    range_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     files = models.ManyToManyField(Files)
-    
+
     def __str__(self):
         return self.range_id
 
+
 class MetaRange(models.Model):
-    meta_id = models.CharField(max_length=10, primary_key=True)
+    meta_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     ranges = models.ManyToManyField(Range)
-    
+
     def __str__(self):
         return self.meta_id
 
+
 class Commit(models.Model):
-    commit_id = models.CharField(max_length=10, primary_key=True)
+    commit_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     meta_id = models.OneToOneField(MetaRange, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        ordering = ['-timestamp']
+        ordering = ["-timestamp"]
 
     def __str__(self):
         return self.commit_id
-    
+
+
 class Branch(models.Model):
     branch_name = models.CharField(max_length=100, primary_key=True)
     created_timestamp = models.DateTimeField(auto_now_add=True)
     updated_timestamp = models.DateTimeField(auto_now=True)
-    commit_id = models.ForeignKey(Commit, on_delete=models.CASCADE, related_name='commits')
+    commit_id = models.ForeignKey(
+        Commit, on_delete=models.CASCADE, related_name="commits"
+    )
 
     class Meta:
-        ordering = ['-created_timestamp']
+        ordering = ["-created_timestamp"]
 
     def __str__(self):
         return self.branch_name
-    
+
+
 class Repo(models.Model):
     repo_name = models.CharField(max_length=100, primary_key=True)
-    description = models.TextField()
-    default_branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name="branch")
+    description = models.TextField(default="")
+    default_branch = models.ForeignKey(
+        Branch, on_delete=models.CASCADE, related_name="branch"
+    )
     gc_bucket = models.URLField(default="")
 
     def __str__(self):
