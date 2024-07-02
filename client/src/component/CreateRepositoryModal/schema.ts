da46da6b2d7d726cabcd4c276630b1273fa04a97
WangYuTengg <wyuteng123@gmail.com>
2024-07-02 21:38:35 +0800

    client: Align createRepositorySchema w backend

--- client/src/component/CreateRepositoryModal/schema.ts
+++ client/src/component/CreateRepositoryModal/schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const createRepositorySchema = z.object({
-  repositoryName: z.string().min(1, "Enter a repository name"),
+  repo_name: z.string().min(1, "Enter a repository name"),
   description: z.string().optional(),
-  storageNamespace: z.string().optional(),
-  defaultBranch: z.string().min(1, "Enter a default branch"),
+  gc_bucket: z.string().url().optional(),
+  default_branch: z.string().min(1, "Enter a default branch"),
 });
 
 export type CreateRepositorySchemaValues = z.infer<
