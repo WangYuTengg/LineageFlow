18aa874455d7eba6d3306924a8bc39752cd90f10
WangYuTengg <wyuteng123@gmail.com>
2024-07-06 17:08:40 +0800

    urls: remove unused endpoints

--- server/myapp/urls.py
+++ server/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .view import CreateRepositoryView
 from .views.UploadObject.view import UploadObjectView
+from .views.onboard.view import OnboardingView
 from .view import (
     CommitView,
     BranchView,
@@ -9,7 +9,6 @@ from .view import (
     GetObjectsView,
     GetRepoView,
 )
-from .views.onboard.view import OnboardingView
 
 urlpatterns = [
     # LOGIN APIs
@@ -19,8 +18,7 @@ urlpatterns = [
     path("getAllRepo/", GetRepoView.as_view(), name="get-repos"),
     path("getFiles/", GetObjectsView.as_view(), name="get-objects"),
     # CREATE APIs
-    path("createRepository/", CreateRepositoryView.as_view(), name="create-repository"),
-    path("onboard/", OnboardingView.as_view(), name="onboard"),
+    path("onboard/", OnboardingView.as_view(), name="onboard"),  # Creating repo
     path("upload/", UploadObjectView.as_view(), name="upload"),
     path("createBranch/", BranchView.as_view(), name="branch"),
     path("commit/", CommitView.as_view(), name="commit"),
