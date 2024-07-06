d2df8b14854b412d6d8d22dd6ca5f2ca1e5448b0
WangYuTengg <wyuteng123@gmail.com>
2024-07-06 17:08:29 +0800

    view: remove unused views

--- server/myapp/view.py
+++ server/myapp/view.py
@@ -1,9 +1,7 @@
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import (
-    RepositorySerializer,
     FilesSerializer,
     RangeSerializer,
     CommitSerializer,
@@ -17,71 +15,6 @@ from .models import (
     Users,
     Repo,
 )
-
-
-class CreateRepositoryView(APIView):
-    def post(self, request):
-        # {"username": "admin", "repo_name": "ADMIN", "default_branch": "master"}
-
-        username = request.data.pop("username")
-        try:
-            user = Users.objects.get(username=username)
-        except Users.DoesNotExist:
-            return Response(
-                {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        repo_instance = RepositorySerializer(data=request.data)
-        if repo_instance.is_valid():
-            repo_instance.save()
-            repo = Repo.objects.get(**repo_instance.validated_data)
-            user.repos.add(repo)
-            user_instance = UserSerializer(instance=user)
-            # TODO create the usertorepo
-            response_data = {
-                "message": "Repo Created Successfully",
-                "data": repo_instance.data,
-            }
-            return Response(response_data, status=status.HTTP_201_CREATED)
-        else:
-            print("Validation errors:", repo_instance.errors)
-        return Response(repo_instance.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class RangeView(APIView):
-#     def get(self, request):
-#         ranges = Range.objects.all()
-#         serializer = RangeSerializer(ranges, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#     def post(self, request):
-#         serializer = RangeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             response_data = {
-#                 "message": "Data Inserted Successfully",
-#                 "data": serializer.data,
-#             }
-#             return Response(response_data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class MetaView(APIView):
-#     def get(self, request):
-#         metas = MetaRange.objects.all()
-#         serializer = MetaRangeSerializer(metas, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#     def post(self, request):
-#         serializer = MetaRangeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             response_data = {
-#                 "message": "Data Inserted Successfully",
-#                 "data": serializer.data,
-#             }
-#             return Response(response_data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FilesView(APIView):
@@ -220,40 +153,3 @@ class GetObjectsView(APIView):
             kvs.append(serializer.data)
 
         return Response({"files": kvs}, status=status.HTTP_200_OK)
-
-
-# class GetRepoView(APIView):
-#     def get(self, request):
-#         username = request.query_params.get("username")
-#         try:
-#             user = Users.objects.get(username=username)
-#         except Users.DoesNotExist:
-#             return Response(
-#                 {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
-#             )
-
-#         repo_ids = UserToRepo.objects.filter(user_id=user.user_id).values_list(
-#             "repo_id", flat=True
-#         )
-#         print("User Repos:", repo_ids)
-
-#         repos = Repo.objects.filter(repo_id__in=repo_ids)
-#         repo_data = [
-#             {
-#                 "repo_id": repo.repo_id,
-#                 "repo_name": repo.repo_name,
-#                 "description": repo.description,
-#                 "storage_bucket_url": repo.bucket_url,
-#             }
-#             for repo in repos
-#         ]
-
-#         response_data = {}
-#         for repo in repo_data:
-#             branches = Branch.objects.filter(repo_id=repo["repo_id"])
-#             serializer = BranchSerializer(branches, many=True)
-#             resp_data = {"details": repo}
-#             resp_data["branches"] = serializer.data
-#             response_data[repo["repo_name"]] = resp_data
-
-#         return Response(response_data, status=status.HTTP_200_OK)
