c6a990e4c437e845452a6b074b7e6f083194ff89
WangYuTengg <wyuteng123@gmail.com>
2024-07-06 17:04:53 +0800

    server: reorganise urls

--- server/myapp/urls.py
+++ server/myapp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .view import CreateRepositoryView
 from .views.UploadObject.view import UploadObjectView
 from .view import (
-    # RangeView,
-    # MetaView,
     CommitView,
     BranchView,
     LoginView,
@@ -14,15 +12,16 @@ from .view import (
 from .views.onboard.view import OnboardingView
 
 urlpatterns = [
-    path("loginAdmin/", LoginView.as_view(), name="login"),
-    path("createRepository/", CreateRepositoryView.as_view(), name="create-repository"),
-    # path("range/", RangeView.as_view(), name="range"),
-    # path("metarange/", MetaView.as_view(), name="meta-range"),
-    path("commit/", CommitView.as_view(), name="commit"),
-    path("createBranch/", BranchView.as_view(), name="branch"),
-    path("onboard/", OnboardingView.as_view(), name="onboard"),
+    # LOGIN APIs
+    path("login/", LoginView.as_view(), name="login"),
     path("signup/", CreateUserView.as_view(), name="sign-up"),
+    # GET APIs
     path("getAllRepo/", GetRepoView.as_view(), name="get-repos"),
     path("getFiles/", GetObjectsView.as_view(), name="get-objects"),
+    # CREATE APIs
+    path("createRepository/", CreateRepositoryView.as_view(), name="create-repository"),
+    path("onboard/", OnboardingView.as_view(), name="onboard"),
     path("upload/", UploadObjectView.as_view(), name="upload"),
+    path("createBranch/", BranchView.as_view(), name="branch"),
+    path("commit/", CommitView.as_view(), name="commit"),
 ]
