b5b4f149cfb79a5eb8278c25a592cc697fe9b8cb
WangYuTengg <wyuteng123@gmail.com>
2024-07-07 14:56:56 +0800

    server: add url for deleteFiles

--- server/myapp/urls.py
+++ server/myapp/urls.py
@@ -3,6 +3,7 @@ from .views.UploadObject.test import Test
 from .views.GetAllRepo.view import GetRepoView
 from .views.GetAllObject.view import GetObjectsView
 from .views.onboard.view import OnboardingView
+from .views.DeleteFiles.view import DeleteFile
 from .views.FetchFiles.view import FetchLatestCommitDataView
 from .view import (
     CommitView,
@@ -24,4 +25,6 @@ urlpatterns = [
     path("upload/", Test.as_view(), name="upload"),
     path("createBranch/", BranchView.as_view(), name="branch"),
     path("commit/", CommitView.as_view(), name="commit"),
+    # DELETE APIs
+    path("deleteFiles/", DeleteFile.as_view(), name="delete-files"),
 ]
