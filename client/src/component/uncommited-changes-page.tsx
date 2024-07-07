30a8620dc16473c1b88c4c4c55cd23be7b3a1114
WangYuTengg <wyuteng123@gmail.com>
2024-07-07 14:57:28 +0800

    client: rename

--- client/src/component/uncommited-changes-page.tsx
+++ client/src/component/uncommited-changes-page.tsx
@@ -8,7 +8,7 @@ import {
   TextInput,
   Button,
 } from "@mantine/core";
-import { Repository, UncommittedChanges } from "../schema";
+import { FileResource, Repository, UncommittedChanges } from "../schema";
 import { IconPlus, IconMinus, IconPencil } from "@tabler/icons-react";
 import { useState } from "react";
 
@@ -25,22 +25,18 @@ export default function UncommittedChangesPage({
 }: Props) {
   const [commitMessage, setCommitMessage] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
   const handleUpload = async () => {
     if (!uncommittedChanges) return;
     setIsLoading(true);
     try {
       const formData = new FormData();
       formData.append("repo", uncommittedChanges.repo);
-      formData.append("branch", uncommittedChanges.branch);
-      formData.append("storage_bucket", uncommittedChanges.storage_bucket);
+      formData.append("branch_name", uncommittedChanges.branch);
       formData.append("commit_message", commitMessage);
       uncommittedChanges.changes.forEach((change) => {
-        if (change.type === "Add") {
-          const file = change.file as File;
-          formData.append("files", file as File);
-          formData.append("relative_paths", file.webkitRelativePath);
-        }
+        const file = change.file as File;
+        formData.append("files", file as File);
+        formData.append("relative_paths", file.webkitRelativePath);
       });
       const response = await fetch("/api/upload/", {
         method: "POST",
@@ -57,6 +53,39 @@ export default function UncommittedChangesPage({
     } catch (error) {
       console.error(error);
       alert("File upload failed");
+    }
+    setIsLoading(false);
+  };
+
+  const handleDelete = async () => {
+    if (!uncommittedChanges) return;
+    setIsLoading(true);
+    try {
+      const data = {
+        repo: uncommittedChanges.repo,
+        branch: uncommittedChanges.branch,
+        commit_message: commitMessage,
+        files_list: uncommittedChanges.changes.map((change) => change.file),
+      };
+      console.log(data);
+      const response = await fetch("/api/deleteFiles/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        alert("Files deleted successfully");
+        onDone();
+      } else {
+        console.error(response.statusText);
+        alert("File deletion failed");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("File deletion failed");
     }
     setIsLoading(false);
   };
@@ -108,7 +137,25 @@ export default function UncommittedChangesPage({
               onChange={(event) => setCommitMessage(event.target.value)}
               placeholder="Enter commit message"
             />
-            <Button color="blue" onClick={handleUpload} loading={isLoading}>
+            <Button
+              color="blue"
+              onClick={() => {
+                if (
+                  uncommittedChanges.changes.some(
+                    (change) => change.type === "Add"
+                  )
+                ) {
+                  handleUpload();
+                } else if (
+                  uncommittedChanges.changes.some(
+                    (change) => change.type === "Delete"
+                  )
+                ) {
+                  handleDelete();
+                }
+              }}
+              loading={isLoading}
+            >
               Commit
             </Button>
           </Group>
