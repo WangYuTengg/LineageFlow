80fb3c1b884853fd87e583564e98104c35580d45
WangYuTengg <wyuteng123@gmail.com>
2024-07-07 19:45:44 +0800

    server: create-branch

--- server/myapp/views/Branch/view.py
+++ server/myapp/views/Branch/view.py
@@ -1,35 +1,68 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from myapp.serializers import (
-    BranchSerializer,
-)
+from django.db import transaction
 from myapp.models import (
+    Repo,
     Branch,
-    Users,
+    Commit,
+    MetaRange,
 )
 
 
 class BranchView(APIView):
     def post(self, request):
         # sample post req: {"username": "admin", "repo_name": "TEST", "parent": "main", "branch_name": "newB"}
-        username = request.data.pop("username")
-        repo = request.data.pop("repo_name")
-        parent = request.data.pop("parent")
+        repo_id = request.data.get("repo_id")
+        parent_branch = request.data.get("parent_branch")
+        branch_name = request.data.get("branch_name")
+        print("Received request data:", request.data)
 
-        repo_list = Users.objects.get(username=username).repos
-        repo_id = repo_list.get(repo_name=repo)
+        source_repo = Repo.objects.get(repo_id=repo_id)
+        print("Source repo found:", source_repo)
 
-        # the new branch will point to that commit
-        old_commit = Branch.objects.get(repo_id=repo_id, branch_name=parent).commit_id
-        new_branch = Branch.objects.create(
-            branch_name=request.data.get("branch_name"),
-            commit_id=old_commit,
-            repo_id=repo_id,
+        source_branch = Branch.objects.get(repo_id=repo_id, branch_name=parent_branch)
+        print("Source branch found:", source_branch)
+
+        source_commit = (
+            Commit.objects.filter(branch=source_branch)
+            .order_by("-created_timestamp")
+            .first()
         )
-        serializer = BranchSerializer(instance=new_branch)
-        response_data = {
-            "message": "Branch Created Successfully",
-            "data": serializer.data,
-        }
-        return Response(response_data, status=status.HTTP_201_CREATED)
+        print("Source commit found:", source_commit)
+
+        source_metarange = MetaRange.objects.get(commit=source_commit)
+        print("Source metarange found:", source_metarange)
+
+        with transaction.atomic():
+            # create new branch
+            new_branch = Branch.objects.create(
+                branch_name=branch_name,
+                repo=source_repo,
+            )
+            print("New branch created:", new_branch)
+
+            # create new commit for new branch
+            new_commit = Commit.objects.create(
+                branch=new_branch,
+                commit_message=f"New branch created from {parent_branch} branch.",
+            )
+            print("New commit created:", new_commit)
+
+            # create new meta range for new commit
+            new_meta_range = MetaRange.objects.create()
+            print("New meta range created:", new_meta_range)
+
+            new_meta_range.ranges.add(*source_metarange.ranges.all())
+            print("Ranges added to new meta range:", new_meta_range.ranges.all())
+
+            new_branch.save()
+            new_commit.save()
+            new_meta_range.save()
+
+        return Response(
+            {
+                "message": "Branch Created Successfully",
+            },
+            status=status.HTTP_201_CREATED,
+        )
